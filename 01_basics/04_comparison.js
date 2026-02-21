15b52637e9abca43b3f5192c6cd1827fc6f01eb6
anshikaagarwal2802-create <anshikaagarwal2802@gmail.com>
2026-02-21 17:37:11 +0000

    concept of comparsions in javascript

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -2,4 +2,25 @@
 // console.log(2>=1)
 // console.log(2!=1)
 // console.log(2==1)
-console.log("2">1)
+// avoid this type of comparison
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// NOTE:
+// the reason is that an equality check == and comparisons > < >= <= work differently. 
+// comparsions convert null to a number,treating it as 0 .
+// that's why (3) null>=0 is true and )(1) null>0 is false .
+// avoid this type of conversion
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+// STRICT CHECK ===( data types ko bhi check krta hai)
+console.log("2"===2);
+
+
+
+
+
+
