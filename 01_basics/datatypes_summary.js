671ffe4ce5bccd56d14976f3ed87c041ad71df7a
anshikaagarwal2802-create <anshikaagarwal2802@gmail.com>
2026-02-22 20:01:06 +0000

    datatypes_in_js

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,20 @@
+// datatypes -> primitive and non-primitive
+// kis tarah se data ko memory me rakha jata ha aur excess kia jata ha us basis par data ka two 
+// categorisation ha  ->primitive && non-primitive
+// Primitive (call by value hote hai)
+// 7 types :  STRING,NUMBER,NULL,UNDEFINED,BOOLEAN,SYMBOL(UNIQUE KE LIY),BIGINT
+
+
+// REFERENCE(NON PRIMITIVE)
+// ARRAYS ,OBJECTS ,FUNCTIONS
+//JavaScript is a dynamically typed programming language.
+// This means that you do not need to explicitly declare the data type of a variable 
+// when you create it, and the type of a variable can change during runtime. 
+// const Score=100;
+// const score=100.3;
+// const isLoggedin=false;
+// const outsidetemp=null;
+ const sym=symbol("foo");
+typeof sym;
+
+
