d930f8d3f66df538647c53b87f32635ba6cd3357
anshikaagarwal2802-create <anshikaagarwal2802@gmail.com>
2026-06-19 21:07:30 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -25,18 +25,49 @@
 // }
 // const result=addtwonumbers(3,4)// function is not giving anything 
 // console.log("Result:",result);// here output is undefined because yah function kuch return nhi kar rha ha functions ke andar bss console.log (means print ho rha ha bss)
-function loginusername(username="sam")// we can give default values
-{    if(!username)
-   // if(username===undefined)//undefined and empty string means false then ham if block ke andar ja hi nhi paynge so ham use krte ha !to reverse 
-    {
-        console.log("please enter username ")
-        return
-    }
-    return `${username} just logged in`
-}
+// function loginusername(username="sam")// we can give default values
+// {    if(!username)
+//    // if(username===undefined)//undefined and empty string means false then ham if block ke andar ja hi nhi paynge so ham use krte ha !to reverse 
+//     {
+//         console.log("please enter username ")
+//         return
+//     }
+//     return `${username} just logged in`
+// }
 //console.log(loginusername("anshika"))
-console.log(loginusername())
-function calculatecartprice(...num1)
+//console.log(loginusername())
+// function calculatecartprice(...num1)
+// {
+//     return num1;
+// }
+// console.log(calculatecartprice(200,400,600,2000));
+// function calculatecartprice(val1,val2,...num1)
+// {
+//     return num1;
+// }
+// console.log(calculatecartprice(200,400,600,2000));
+const user=
 {
-    
+    username:"anshika",
+    price:199
 }
+function handleobject(anyobject)
+{
+
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+
+}
+//handleobject(user) or
+handleobject(
+    {
+       username:"anshika",
+       price:199 
+    }
+)
+const mynewArray=[200,400,600,800]
+function returnsecondvalue(getarray)
+{
+    return getarray[1]
+}
+//console.log(returnsecondvalue(mynewArray)) or
+console.log(returnsecondvalue([200,400,600,800]))
