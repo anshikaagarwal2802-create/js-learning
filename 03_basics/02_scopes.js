efa0b3105c3f76f6a163f9b4a2437d18936c932c
anshikaagarwal2802-create <anshikaagarwal2802@gmail.com>
2026-06-20 07:39:40 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,16 @@
+//var c =300
+let a=300
+if(true)
+{
+    let a=20
+    const b=30
+    var c=40
+    console.log("Inner:",a)
+
+}
+ console.log(a)
+// console.log(b)
+//console.log(c)
+//global scope browser me and code environment me node ke through
+
+ 
